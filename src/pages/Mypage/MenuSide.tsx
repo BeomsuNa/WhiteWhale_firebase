13875c4771d77c0d2ce55c01826751e0099bf7ae
skqjatn6788@gmail.com <123361606+BeomsuNa@users.noreply.github.com>
2025-11-14 23:21:42 +0900

    1.마이페이지 수정중입니다.
    1. 마이페이지의 왼쪽 사이드탭 개설 및 각 페이지 제작중

--- src/pages/Mypage/MenuSide.tsx
+++ src/pages/Mypage/MenuSide.tsx
@@ -2,13 +2,48 @@ import React from 'react';
 
 const MenuSide = () => {
   return (
-    <div className=" h-auto flex">
-      <ul>
-        <li>주문내역</li>
-        <li>주문내역</li>
-        <li>주문내역</li>
-      </ul>
-    </div>
+    <aside className="w-full px-6 py-8 ">
+      <h2 className="font-bold text-lg mb-7 border-b pb-3">마이페이지</h2>
+
+      <section className="mb-7">
+        <h3 className="font-bold text-base mb-3 border-b border-gray-200 pb-2">
+          쇼핑정보
+        </h3>
+        <ul className="space-y-2 text-sm  pl-2">
+          <li>- 주문목록/배송조회</li>
+          <li>- 취소/반품/교환 내역</li>
+          <li>- 환불/입금 내역</li>
+          <li>- 찜리스트</li>
+        </ul>
+      </section>
+
+      <section className="mb-7">
+        <h3 className="font-bold text-base mb-3 border-b  pb-2">혜택관리</h3>
+        <ul className="space-y-2 text-sm  pl-2">
+          <li>- 쿠폰</li>
+          <li>- 예치금</li>
+          <li>- 마일리지</li>
+        </ul>
+      </section>
+
+      <section className="mb-7">
+        <h3 className="font-bold text-base mb-3 border-b border-gray-200 pb-2">
+          고객센터
+        </h3>
+        <ul className="space-y-2 text-smpl-2">
+          <li>- 1:1 문의</li>
+        </ul>
+      </section>
+
+      <section>
+        <h3 className="font-bold text-base mb-3 border-b border-gray-200 pb-2">
+          회원정보
+        </h3>
+        <ul className="space-y-2 text-sm  pl-2">
+          <li className=" font-bold">- 회원정보 변경</li>
+        </ul>
+      </section>
+    </aside>
   );
 };
 
--- src/pages/Mypage/MyPage.tsx
+++ src/pages/Mypage/MyPage.tsx
@@ -4,20 +4,21 @@ import MenuSide from './MenuSide';
 function MyPage() {
   return (
     <div
-      className="w-full 
- min-w-[460px]           
-  sm:min-w-[500px]        
-  md:min-w-[750px]        
-  lg:min-w-[850px]       
-  xl:min-w-[900px] 
-  max-w-[1260px]     
-  mx-auto flex flex-row items-center  
-  gap-6 mt-12 mb-36 "
+      className=" w-full 
+        min-w-[460px]          
+        sm:min-w-[500px]        
+        md:min-w-[750px]        
+        lg:min-w-[850px]        
+        xl:min-w-[900px] 
+        max-w-[1260px]     
+        mx-auto flex flex-row items-start   
+        gap-0 mt-12 mb-36
+      "
     >
-      <div className="flex w-1/4 ">
+      <div className="flex w-1/4 h-full pr-8 border-r border-gray-200 ">
         <MenuSide />
       </div>
-      <div className="flex w-full ">
+      <div className="flex flex-1 pl-8 ">
         <DliveryState />
       </div>
     </div>
