4fa94cb790ab43b3e170f1e209514a107e05cfa0
skqjatn6788@gmail.com <123361606+BeomsuNa@users.noreply.github.com>
2025-11-17 23:36:48 +0900

    사이드메뉴 탭 수정중
    디자인 시가성 추가

--- src/pages/Mypage/MenuSide.tsx
+++ src/pages/Mypage/MenuSide.tsx
@@ -1,15 +1,21 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 const MenuSide = () => {
   return (
-    <aside className="w-full px-6 py-8 ">
-      <h2 className="font-bold text-lg mb-7 border-b pb-3">마이페이지</h2>
+    <aside className="w-full px-6 ml-6 py-8 bg-[rgb(182,180,180)] rounded-md">
+      <h2 className="font-bold text-lg text-gray-700 mb-7 border-b border-gray-500 pb-3 ">
+        마이페이지
+      </h2>
 
       <section className="mb-7">
-        <h3 className="font-bold text-base mb-3 border-b border-gray-200 pb-2">
-          쇼핑정보
-        </h3>
-        <ul className="space-y-2 text-sm  pl-2">
+        <div className="flex flex-row ml-10">
+          <h3 className="font-bold text-base text-gray-700 border-t pt-2 mb-2 border-gray-200 ">
+            쇼핑
+          </h3>
+          <h3 className="font-bold text-base text-gray-700 pt-2 mb-2">정보</h3>
+        </div>
+        <ul className="flex flex-col items-start space-y-2 text-sm pl-2 ml-12 ">
           <li>- 주문목록/배송조회</li>
           <li>- 취소/반품/교환 내역</li>
           <li>- 환불/입금 내역</li>
@@ -18,8 +24,30 @@ const MenuSide = () => {
       </section>
 
       <section className="mb-7">
-        <h3 className="font-bold text-base mb-3 border-b  pb-2">혜택관리</h3>
-        <ul className="space-y-2 text-sm  pl-2">
+        <div className="flex  ml-10">
+          <div className="font-bold text-base text-gray-700 border-t pt-2 mb-2 border-gray-100 ">
+            <h3>&nbsp; &nbsp;&nbsp; &nbsp;</h3>
+          </div>
+          <h3 className="font-bold text-base text-gray-700 pt-2 mb-2">
+            <span className="border-t-2 border-gray-100 pt-2">쇼핑</span>정보
+          </h3>
+        </div>
+        <ul className="flex flex-col items-start space-y-2 text-sm pl-2 ml-12 ">
+          <li>- 주문목록/배송조회</li>
+          <li>- 취소/반품/교환 내역</li>
+          <li>- 환불/입금 내역</li>
+          <li>- 찜리스트</li>
+        </ul>
+      </section>
+
+      <section className="mb-7">
+        <div className="flex flex-row ml-10">
+          <h3 className="font-bold text-base border-t pt-2 mb-2 border-gray-200 ">
+            혜택
+          </h3>
+          <h3 className="font-bold text-base pt-2 mb-2 ">관리</h3>
+        </div>
+        <ul className="flex flex-col items-start space-y-2 text-sm pl-2 ml-12">
           <li>- 쿠폰</li>
           <li>- 예치금</li>
           <li>- 마일리지</li>
@@ -27,20 +55,54 @@ const MenuSide = () => {
       </section>
 
       <section className="mb-7">
-        <h3 className="font-bold text-base mb-3 border-b border-gray-200 pb-2">
-          고객센터
-        </h3>
-        <ul className="space-y-2 text-smpl-2">
+        <div className="flex flex-row ml-10">
+          <h3 className="font-bold text-base border-t pt-2 mb-2 border-gray-200 ">
+            고객
+          </h3>
+          <h3 className="font-bold text-base pt-2 mb-2 ">센터</h3>
+        </div>
+        <ul className="flex flex-col items-start space-y-2 text-sm pl-2 ml-12">
           <li>- 1:1 문의</li>
         </ul>
       </section>
 
       <section>
-        <h3 className="font-bold text-base mb-3 border-b border-gray-200 pb-2">
-          회원정보
-        </h3>
-        <ul className="space-y-2 text-sm  pl-2">
-          <li className=" font-bold">- 회원정보 변경</li>
+        <div className="flex flex-row ml-10">
+          <h3 className="font-bold text-base border-t pt-2 mb-2 border-gray-200 ">
+            회원
+          </h3>
+          <h3 className="font-bold text-base pt-2 mb-2 ">정보</h3>
+        </div>
+        <ul className="flex flex-col items-start space-y-2 text-sm pl-2 ml-12">
+          <li>
+            <Link to="/mypage/edit">- 회원정보 변경</Link>
+          </li>
+          <li>- 회원탈퇴</li>
+          <li>- 배송지정보</li>
+        </ul>
+      </section>
+
+      <section>
+        <div className="flex flex-row ml-10">
+          <h3 className="font-bold text-base border-t pt-2 mb-2 border-gray-200 ">
+            나의
+          </h3>
+          <h3 className="font-bold text-base pt-2 mb-2 ">상품정보</h3>
+        </div>
+        <ul className="flex flex-col items-start space-y-2 text-sm pl-2 ml-12">
+          <li>- 나의 상품정보</li>
+        </ul>
+      </section>
+
+      <section>
+        <div className="flex flex-row ml-10">
+          <h3 className="font-bold text-base border-t pt-2 mb-2 border-gray-200 ">
+            나의
+          </h3>
+          <h3 className="font-bold text-base pt-2 mb-2 ">상품후기</h3>
+        </div>
+        <ul className="flex flex-col items-start space-y-2 text-sm pl-2 ml-12">
+          <li>- 나의 상품후기</li>
         </ul>
       </section>
     </aside>
